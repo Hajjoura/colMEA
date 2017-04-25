84f1678f7ec2d6e8410e06d3da21df271df401ac
Hejer Krichene <hejer.krichene@esprit.tn>
2017-04-25 16:26:46 +0200

    adding service interval

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7f/a00b4e26c32900171e5ac19278cac36c
@@ -0,0 +1,20 @@
+package com.supmeca.colMEA.business;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class IntervalService
+ */
+@Stateless(name = "IntervalServices")
+@LocalBean
+public class IntervalService implements IntervalServiceRemote, IntervalServiceLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public IntervalService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
--- ColMEA.Ejb/src/main/java/com/supmeca/colMEA/business/IntervalServiceLocal.java
+++ ColMEA.Ejb/src/main/java/com/supmeca/colMEA/business/IntervalServiceLocal.java
@@ -1,8 +1,17 @@
 package com.supmeca.colMEA.business;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import com.supmeca.colMEA.domain.Interval;
 
 @Local
 public interface IntervalServiceLocal {
-
+	public void CreateInterval( Interval Interval) ;
+	public void EditInterval (Interval Interval);
+	public void removeInterval(Interval Interval);
+	public Interval findIntervalById(int id);
+	public List<Interval> findAllIntervals() ;
+	public void DeleteInterval(int id);
 }
