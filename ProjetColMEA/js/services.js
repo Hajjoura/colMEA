91863c5772af0295500f41aea4dc69cbf4dec869
Hejer krichene <hejer.krichene@hotmail.fr>
2017-06-18 14:17:13 +0200

    front end moved

--- ProjetColMEA/js/services.js
+++
@@ -1,9 +0,0 @@
-var services = angular.module('ColMEA.services', ['ngResource']);
-var baseUrl = 'http://localhost:8080/ColMEA.Web/';
-
-
-services.factory('AuthFactory',function($resource){
-
-    return $resource(baseUrl + '/Users/auth/:login/:password');
-});
-
