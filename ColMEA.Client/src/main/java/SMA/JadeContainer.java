eca96c5f37175dad4e8287d0b8870ac16bdc7e4d
Hejer krichene <hejer.krichene@hotmail.fr>
2017-06-19 17:02:54 +0200

    jade container update

--- ColMEA.Client/src/main/java/SMA/JadeContainer.java
+++ ColMEA.Client/src/main/java/SMA/JadeContainer.java
@@ -1,5 +1,24 @@
 package SMA;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
 
 public class JadeContainer {
-
+	public static void main(String[] args) {
+		try {
+			Runtime rt = Runtime.instance();
+			ProfileImpl pc = new ProfileImpl(false); // false pour dire que ce n'est pas MainContainer
+			pc.setParameter(ProfileImpl.MAIN_HOST, "localhost");
+			AgentContainer ac = rt.createAgentContainer(pc);
+							
+			//Deployer un agent
+			AgentController agentController = ac.createNewAgent("DesignAgent", "Agent.DesignAgent", new Object[]{"XML"});
+			agentController.start();
+										
+		} catch (ControllerException e) {
+			e.printStackTrace();
+		}
+}
 }
